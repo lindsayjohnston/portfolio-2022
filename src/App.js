8e884ca343ed88de823d54af6de9f2516a4a3232
Lindsay Johnston <lindsaykjohnston@outlook.com>
2022-08-13 19:59:26 -0700

    Initialized react app. Cleaned up unnecessary files. Added  base font-size to index.css.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="app-header">
+        <h1>Lindsay K. Johnston</h1>
+        <h2>test</h2>
+        <h3>test</h3>
+        <h4>test</h4>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
