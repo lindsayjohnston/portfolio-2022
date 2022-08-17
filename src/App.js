65b8d56ba01b3bfa8ac8acaaaefe61a8a1f65506
Lindsay Johnston <lindsaykjohnston@outlook.com>
2022-08-16 17:58:06 -0700

    Created tiles with dummy content and dummy titles to test layout.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
+import Tile from './Tile/Tile';
+
+let testArray = Array(12).fill(0);
 
 function App() {
   return (
     <div className="App">
       <header className="app-header">
         <h1>Lindsay K. Johnston</h1>
-        <h2>test</h2>
-        <h3>test</h3>
-        <h4>test</h4>
-        <p className='secondary-font'> This is an example of secondary font</p>
-
       </header>
+      <main>
+        { 
+          testArray.map(element =>{
+            return(<Tile/>)
+          })
+        }
+      </main>
     </div>
   );
 }
---
+++ src/Tile/Tile.js
@@ -0,0 +1,12 @@
+import './Tile.css';
+
+const Tile = (props) =>{
+    return(
+        <div className="tile">
+            <div className="content"></div>
+            <h2>Test Title</h2>
+        </div>
+    )
+}
+
+export default Tile;
