9edbe754e7626b52f4a910f3216d9b28c08dac8d
Lindsay Johnston <lindsaykjohnston@outlook.com>
2022-08-19 15:34:02 -0700

    Added in some test images to get the tiles to behave correctly with picture elements.

--- src/routes/Work/Work.js
+++ src/routes/Work/Work.js
@@ -3,22 +3,46 @@ import Tile from '../../Tile/Tile.js';
 
 let testArray = Array(12).fill(0);
 
-const workImgs = {
-    image: {
-        title: "blah"
-    }
+const workMedia = {
+    creative: {
+        title: "Creative",
+        type: "img",
+        src: "../../media/work/sad-mode-album-cover_768x506.webp"
+    },
+    creative2: {
+        title: "Creative2",
+        type: "img",
+        src: "../../media/work/sad-mode-album-cover_768x506.webp"
+    },
+    // web:{
+    //     title: "Web Development"
+    // },
+    // music:{
+    //     title: "Music"
+    // },
+    // corporate:{
+    //     title: "Corporate"
+    // },
+    // apparel:{
+    //     title: "Apparel"
+    // },
+    
 }
 
 const Work = (props) => {
     return (
         <div className="work-page page-content">
-            {Object.keys(workImgs).map(key => {
+            {Object.keys(workMedia).map(key => {
          
                 return (
                     <Tile key={key}>
-                        {workImgs[key].title}
+                    {/* picture is a much newer addition to the language. Its main goal is to let us load different sources depending on resolution or support for a given image format. */}
+                        <picture>
+                            <source srcSet='../sad-mode-album-cover_768x506.webp' type="image/webp"/>
+                            <source srcSet='../sad-mode-album-cover_768x506.webp' type="image/jpg"/>
+                            <img src= '../sad-mode-album-cover-768x506.jpg'></img>
+                        </picture>
                     </Tile>
-
                 )
             }
 
