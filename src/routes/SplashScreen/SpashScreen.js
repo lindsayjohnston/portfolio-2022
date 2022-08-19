009eae406d0e01d241a3d7e0147446cb07892cbc
Lindsay Johnston <lindsaykjohnston@outlook.com>
2022-08-18 20:02:40 -0700

    Removed buttons. Consolidated fade out effects so that screen fades out too and then is removed from DOM.

--- src/routes/SplashScreen/SpashScreen.js
+++ src/routes/SplashScreen/SpashScreen.js
@@ -3,31 +3,27 @@ import './SplashScreen.css';
 
 const SplashScreen = (props) => {
     const [screenShown, setScreenShown] = useState(true);
-    const fadeOut = () =>{
-        //do a thing
+
+    const closeScreen = ()=>{
+        //start fading out title
         const title = document.getElementById('splash-title-div');
         title.classList.remove('fade-in');
         title.classList.add('fade-out');
-    }
-    const fadeIn = () =>{
-        //do a thing
-        const title = document.getElementById('splash-title-div');
-        title.classList.remove('fade-out');
-        title.classList.add('fade-in');
+        //start fading out screen .5s later
+        const screen = document.getElementById('splash-screen');
+        setTimeout(()=>screen.classList.add('fade-out'), 500);
+        //when everything's faded, remove the screen from dom
+        setTimeout(()=>setScreenShown(false), 3000)
     }
 
-    //if content not loaded return(props.children)
     if (screenShown) {
         return (
-            <div className='splash-screen'>
+            <div id="splash-screen" className='splash-screen' onClick={closeScreen}>
                 <div className='splash-screen-content flex-column'>
                     <div id='splash-title-div'className='splash-title flex-column fade-in'>
                         <h1>lindsay k.</h1>
                         <h1>johnston</h1>
                     </div>
-                    <button onClick={() => fadeOut()}>Fade Out</button>
-                    <button onClick={() => fadeIn()}>Fade In</button>
-                    <button onClick={() => setScreenShown(false)}>hide</button>
                 </div>
             </div>
         )
