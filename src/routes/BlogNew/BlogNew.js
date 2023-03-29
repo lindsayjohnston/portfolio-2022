8c654337b07d6b43f3568cff4942b8e78a84d9fe
ecljohnston <ljohnston@eclipsemining.com>
2023-03-28 18:11:52 -0700

    Added error handling and loader for blog post GET request.

--- src/routes/BlogNew/BlogNew.js
+++ src/routes/BlogNew/BlogNew.js
@@ -32,19 +32,23 @@ const BlogNew = () => {
     //get blogs from server
     const baseURL = "https://mongodb-test-ziu4.onrender.com/posts"
     const [blogs, setBlogs] = useState(null)
+    const [blogsError, setBlogsError] = useState(false)
 
     useEffect(() => {
         axios.get(baseURL).then((response) => {
             setBlogs(response.data)
-        })
+        }).catch(setBlogsError(true))
     }, [])
 
     return (
         <div className="blog-page page-content">
             <h2>Blog</h2>
-            {blogs &&
-                blogs.map((blogData, index) => {
-                    let sectionClass = "section-odd"
+            {!blogs && !blogsError && <h3>Loading...</h3>}
+
+            {blogs && 
+                <>
+                {blogs.map((blogData, index) => {
+                        let sectionClass = "section-odd"
                     if (index % 2 === 0) {
                         sectionClass = "section-even"
                     }
@@ -52,21 +56,28 @@ const BlogNew = () => {
                         sectionClass = "highlighted-blog"
                     }
                     return (
-                        <Link to={`/blog/${blogData._id}`} key={blogData._id}>
-                            <section className={sectionClass}>
-                                <div className="text-area">
-                                    <h3>{blogData.title}</h3>
-                                    <p>{blogData.snippet}</p>
-                                    <p><strong>{blogData.date}</strong></p>
-                                </div>
-                                <img alt={blogData.imgAlt} src={blogData.thumbnail} />
-                            </section>
-                        </Link>
+                    <Link to={`/blog/${blogData._id}`} key={blogData._id}>
+                        <section className={sectionClass}>
+                            <div className="text-area">
+                                <h3>{blogData.title}</h3>
+                                <p>{blogData.snippet}</p>
+                                <p><strong>{blogData.date}</strong></p>
+                            </div>
+                            <img alt={blogData.imgAlt} src={blogData.thumbnail} />
+                        </section>
+                    </Link>
                     )
                 })}
-            <div>
-                <h3>More posts coming soon!</h3>
-            </div>
+                    <div>
+                        <h3>More posts coming soon!</h3>
+                    </div>
+                </>
+            }
+            {
+                blogsError &&
+                <p>Shoot! Something went wrong. Please try refreshing.</p>
+            }
+
         </div>
     )
 }
