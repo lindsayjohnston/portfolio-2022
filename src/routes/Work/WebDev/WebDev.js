5c696102b87d89cbc766992c92c3d1059ca7d054
Lindsay Johnston <lindsaykjohnston@outlook.com>
2023-08-15 15:16:00 -0700

    Added section for Web Scraper App.

--- src/routes/Work/WebDev/WebDev.js
+++ src/routes/Work/WebDev/WebDev.js
@@ -5,9 +5,9 @@ const WebDev = () => {
     //TODO:
     //spellcheck
     //make mockups for Chipotle, add section for chat server
-    const handleBlogEditorButtonClick = (e) => {
+    const handleGatedContentButtonClick = (e) => {
         e.preventDefault()
-        alert("The blog editor requires credentials to access. Please contact lindsaykjohnston@outlook.com for a demo!")
+        alert("This app requires credentials to access. Please contact lindsaykjohnston@outlook.com for a demo!")
     }
 
     return (
@@ -17,11 +17,25 @@ const WebDev = () => {
                 <section className="section-odd flex-column">
                     <p>When my music career came to an abrupt halt during the COVID pandemic, I used my extra time to teach myself computer programming. I am enamored with the mix of logic and artistry that web development offers. I love making pixel perfect websites that are sleek, responsive, and accessible.</p>
                 </section>
+                <section className="section-odd flex-column">
+                    <img alt="web scraper mockup"
+                        src="../media/work/blog-editor-mockup768x508.png"></img>
+                    <button onClick={handleGatedContentButtonClick}
+                        className="secondary-link-style link-eque-button">Stock Data Web Scraper</button>
+                    <a target="_blank" rel="noreferrer" className="secondary-link-style"
+                        href="#">GitHub Repo</a>
+                    <p>
+                        Unable to find the exact stock screens I wanted to
+                        build an investment portfolio, I developed a JavaScript web-scraping app to do the job for me. I feed the app a list of stock tickers and it outputs the data I need into a CSV file.
+                    </p>
+                </section>
                 <section className="section-even flex-column">
                     <img alt="Blog editor mockup"
                         src="../media/work/blog-editor-mockup768x508.png"></img>
-                    <button onClick={handleBlogEditorButtonClick}
+                    <button onClick={handleGatedContentButtonClick}
                         className="secondary-link-style link-eque-button">MERN Stack Blog Editor</button>
+                    <a target="_blank" rel="noreferrer" className="secondary-link-style"
+                        href="#">GitHub Repo</a>
                     <p>
                         This full-stack blog-editing app allows me to write, preview, and submit blog posts for this website.
                         It features my own hand-rolled markdown language as well as a REST API built with Express.js and MongoDB.
@@ -33,7 +47,7 @@ const WebDev = () => {
                         src="../media/work/chipotle-clone-device-mockup768x508.png"></img>
                     <a target="_blank" rel="noreferrer" className="secondary-link-style"
                         href="https://main.d25r1kk5mc9ae9.amplifyapp.com/">Online Ordering App - Responsiveness Study</a>
-                    <p>React components produce dynamic content cards to showcase each item on this restaurant's menu. 
+                    <p>React components produce dynamic content cards to showcase each item on this restaurant's menu.
                         Users can choose ingredients, add or remove items from their cart, and place an order online.</p>
                 </section>
                 <section className="section-even flex-column">
